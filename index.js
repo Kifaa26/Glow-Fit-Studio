6c557e4f7ca8a21c845e3608ce0ccc371896616a
Kifaa26 <akifahpalekar@gmail.com>
2024-08-30 15:41:30 +0200

    update index

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
-//
+import path from "path";   
+import { userRouter, express } from "./controller/UserController.js";
+import { errorHandling } from "./middleware/ErrorHandling.js";
+import cors from 'cors'
+import { instructorRouter } from "./controller/InstructorsController.js";
+
+const app = express()
+const port = +process.env.PORT || 4000
+
+// Middleware
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*")
+    res.header("Access-Control-Allow-Credentials", "true")
+    res.header("Access-Control-Allow-Methods", "*")
+    res.header("Access-Control-Request-Methods", "*")
+    res.header("Access-Control-Allow-Headers", "*")
+    res.header("Access-Control-Expose-Headers", "Authorization")
+    next()
+  })
+app.use('/users', userRouter)
+app.use('/instructors', instructorRouter)
+app.use(
+  express.static("./static"),
+  express.json(),
+  express.urlencoded({
+    extended: true
+    }),
+    cors()
+)
+// Endpoint
+app.get("^/$|/glowfitstudio", (req, res) => {
+  res.status(200).sendFile(path.resolve("./static/html/index.html"))
+})
+app.get('*', (req, res) => {    
+    res.json({
+      status: 404,
+      msg: 'Resource not found'
+    })
+  })
+app.use(errorHandling)
+app.listen(port, () => {
+    console.log(`Live on Port: ${port}`)
+  })

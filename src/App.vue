42513bcc02ae04b946cb56ab3db402a85495d2bf
Kifaa26 <akifahpalekar@gmail.com>
2024-09-05 12:49:50 +0200

    Footer

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,23 @@
 <template>
-  <NavBar />
-  <main>
-    <router-view />
-  </main>
-  <!-- <Footer/> -->
+  <div class="wrapper">
+    <NavBar />
+    <main>
+      <router-view />
+    </main>
+    <Footer/>
+
+  </div>
 </template>
 
 <script>
 
 import NavBar from '@/components/NavBar.vue';
-// import Footer from '@/components/Footer.vue';
+import Footer from '@/components/Footer.vue';
 
 export default {
 components: {
-  NavBar
-  // Footer
+  NavBar,
+  Footer
 }
 }
 </script>

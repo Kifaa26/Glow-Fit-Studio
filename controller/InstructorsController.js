da1077249b99b6c53cda099355869fab9e34f350
Kifaa26 <akifahpalekar@gmail.com>
2024-09-12 11:54:09 +0200

    edits

--- controller/InstructorsController.js
+++ controller/InstructorsController.js
@@ -5,27 +5,27 @@ import { instructors } from '../model/index.js'
 const instructorRouter = express.Router()
 instructorRouter.use(bodyParser.json())
 
-instructorRouter.get('/', (req, res) => {
+instructorRouter.get('/instructors', (req, res) => {
     instructors.fetchInstructors(req, res)
 })
 
-instructorRouter.get('/latest',(req , res) => {
-    instructors.latestInstructors(req, res)
-})
-
-instructorRouter.get('/:id',(req, res) => {
+instructorRouter.get('/instructor/:id', (req, res) => {
     instructors.fetchInstructor(req, res)
 })
 
-instructorRouter.post('/addinstructor',(req, res) => {
+instructorRouter.get('/instructor/latest',(req , res) => {
+    instructors.latestInstructors(req, res)
+})
+
+instructorRouter.post('/instructor/addInstructors',(req, res) => {
     instructors.addInstructor(req, res)
 })
 
-instructorRouter.patch('/update/:id',(req, res) => {
+instructorRouter.patch('/instructor/update/:id',(req, res) => {
     instructors.updateInstructor(req, res)
 })
 
-instructorRouter.delete('/delete/:id',(req, res) => {
+instructorRouter.delete('/instructor/delete/:id',(req, res) => {
     instructors.deleteInstructor(req, res)
 })
 
